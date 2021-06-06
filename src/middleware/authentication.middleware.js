1085510fa8134451ad5f4b8b6146e6c3b8a5e96e
vladimirogaston <vladimirogaston@gmail.com>
2021-06-06 08:53:15 -0300

    add inmemory db for test purpouse

--- src/middleware/authentication.middleware.js
+++ src/middleware/authentication.middleware.js
@@ -1,15 +1,17 @@
-const HttpException = require('../middleware/HttpException');
-const moment = require('moment');
-const jwt = require('jsonwebtoken');
+const HttpException = require('../middleware/HttpException')
+const moment = require('moment')
+const dotenv = require('dotenv')
+const jwt = require('jsonwebtoken')
 
 const authenticate = (...roles) => {
     return async function (req, res, next) {
-        if(!req.headers['token']) throw new HttpException(404, 'Bad formed header exception');
-        const token = req.headers['token'];
-        const payload = jwt.decode(token, 'supersecret');
-        if(payload.expiredAt < moment().unix()) throw HttpException(500, 'El token a expirado');
-        next();
+        if(!req.headers['Authentication']) throw new HttpException(404, 'Bad formed header exception')
+        const token = req.headers['Authentication']
+        dotenv.config()
+        const payload = jwt.decode(token, process.env.SECRET_JWT)
+        if(payload.expiredAt < moment().unix()) throw HttpException(500, 'El token a expirado')
+        next()
     }
 }
 
-module.exports = { authenticate };
+module.exports = { authenticate }
