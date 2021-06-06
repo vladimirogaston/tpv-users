20086614fb4252917589f93b270372554347955b
vladimirogaston <vladimirogaston@gmail.com>
2021-06-06 00:04:59 -0300

    add tests

--- test/users.test.js
+++ test/users.test.js
@@ -17,4 +17,28 @@ describe('Tasks API', ()=>{
             })
         })
     })
+    /* Test the GET Login route */
+    describe('GET /api/v0/users/login', ()=>{
+        it('It should get jwt token', (done)=>{
+            chai.request(app)
+            .post('/api/v0/users/login')
+            .send({
+                username: 'augustopinochet',
+                password: 'ilovekillingpeople'
+            })
+            .end((err,res)=>{
+                res.should.have.status(401)
+                done()
+            })
+        })
+    })
+    /* Test DELETE route */
+    describe('DELETE /api/v0/users', ()=>{
+        it('It should delete one user', (done)=>{
+            chai.request(app)
+            .delete('/api/v0/users/login' + 1)
+            //.set({ "Authorization": `Bearer ${token}` })
+            .set({ "Authorization": `Bearer ${token}` })
+        })
+    })
 })
