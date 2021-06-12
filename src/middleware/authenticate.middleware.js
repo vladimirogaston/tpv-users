208abd7c81ae9146409d0f67e535f54c88b2fbc2
vladimirogaston <vladimirogaston@gmail.com>
2021-06-12 13:51:00 -0300

    fix bugs on authenticate.middleware remove unnecesary authorize.middleware

--- src/middleware/authenticate.middleware.js
+++ src/middleware/authenticate.middleware.js
@@ -1,23 +1,32 @@
-const { UserDAO } = require('../models/db')
 const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv')
 const HttpException = require('./HttpException')
+const userRoles = require('../models/user.roles')
 
-module.exports = async function auth(req, res, next) {
-    const authHeader = req.headers['authorization']
-    if(!authHeader || !authHeader.startsWith('Bearer')) {
-        throw new HttpException(403, 'Bad formed header exception')
-    }
+module.exports = (rol) => {
+    return async function (req, res, next) {
+        const authHeader = req.headers['authorization']
+        if (!authHeader || !authHeader.startsWith('Bearer')) {
+            throw new HttpException(403, 'Bad formed header exception')
+        }
 
-    try {
         dotenv.config()
         const secretKey = process.env.SECRET_JWT
         const token = authHeader && authHeader.split(' ')[2]
-        const decoded = jwt.verify(token, secretKey);
-        const userRole = UserDAO.findByPk(Number(decoded.id))
-        req.body.role = userRole
+        
+        let decoded;
+        try {
+            decoded = jwt.verify(token, secretKey)
+        } catch (err) {
+            res.status(500).send('ERROR VERIFY')
+        }
+        
+        const userROLE = decoded.role
+        if (rol) {
+            if (rol !== userROLE) {
+                res.status(500).send('ERROR VERIFY ROLE')
+            }
+        }
         next()
-    }catch(err){
-        res.status(500).send('ERROR VERIFY')
     }
 }
