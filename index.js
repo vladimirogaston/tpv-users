21e0a69d141399ba95e03f9dccea518251e7f78a
vladimirogaston <vladimirogaston@gmail.com>
2021-06-12 13:14:35 -0300

    fix bug on index.js on swaggerJsDoc duplication removed

--- index.js
+++ index.js
@@ -13,24 +13,6 @@ require('./src/models/db')
 const PORT = process.env.PORT || 4000
 
 const specs = swaggerJsDoc(swaggerOptions)
-const options = {
-	definition: {
-		openapi: "3.0.0",
-		info: {
-			title: "Library API",
-			version: "1.0.0",
-			description: "A simple Express Library API",
-		},
-		servers: [
-			{
-				url: "http://localhost:4000/api/v0",
-			},
-		],
-	},
-	apis: ["./src/routes/*.js"],
-};
-
-const specs = swaggerJsDoc(options)
 const app = express()
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs))
 app.use(cors())
